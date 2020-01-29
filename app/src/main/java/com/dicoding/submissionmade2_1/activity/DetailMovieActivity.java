5beae6c3c6964d93757a1f1db09f47945c886696
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-01-29 13:36:58 +0800

    bug Cannot observe null data - MVVM with Livedata

--- app/src/main/java/com/dicoding/submissionmade2_1/activity/DetailMovieActivity.java
+++ app/src/main/java/com/dicoding/submissionmade2_1/activity/DetailMovieActivity.java
@@ -1,18 +1,29 @@
 package com.dicoding.submissionmade2_1.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.dicoding.submissionmade2_1.Item.FavoriteMovie;
 import com.dicoding.submissionmade2_1.Item.Movie;
 import com.dicoding.submissionmade2_1.R;
+import com.dicoding.submissionmade2_1.ViewModel.FavoriteMovieViewModel;
+
+import java.util.List;
 
 public class DetailMovieActivity extends AppCompatActivity {
 
     public static final String EXTRA_MOVIE = "extra_movie";
+    FavoriteMovieViewModel favoriteMovieViewModel;
+    int idMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +33,11 @@ public class DetailMovieActivity extends AppCompatActivity {
         TextView tvTitle = findViewById(R.id.tv_title_received),
                 tvDescription = findViewById(R.id.txt_description_received);
         ImageView imgPoster = findViewById(R.id.img_received);
+        Button btn_favorite_this_movie = findViewById(R.id.favorite_this_movie);
 
 
         try {
             Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
-
 
             tvTitle.setText(movie.getTitle());
             tvDescription.setText(movie.getDescription());
@@ -36,10 +47,22 @@ public class DetailMovieActivity extends AppCompatActivity {
                     .placeholder(R.color.colorAccent)
                     .dontAnimate()
                     .into(imgPoster);
-//            imgPoster.setImageResource(movie.getPoster());
+            idMovie = movie.getIdMovie();
+            Log.d("id movie", String.valueOf(idMovie));
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
 
+
+        favoriteMovieViewModel = ViewModelProviders.of(this).get(FavoriteMovieViewModel.class);
+        favoriteMovieViewModel.getAllFavoriteMovieById(idMovie).observe(this, new Observer<List<FavoriteMovie>>() {
+            @Override
+            public void onChanged(List<FavoriteMovie> favoriteMovies) {
+                if (favoriteMovies.size() == 0) {
+                    Toast.makeText(DetailMovieActivity.this, "tak berisi", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
