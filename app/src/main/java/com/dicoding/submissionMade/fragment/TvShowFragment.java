1ff91a0e18d4b1fa474714c07c21ee343ad1821d
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-02-25 22:34:57 +0800

    membuat search untuk tv show

--- app/src/main/java/com/dicoding/submissionMade/fragment/TvShowFragment.java
+++ app/src/main/java/com/dicoding/submissionMade/fragment/TvShowFragment.java
@@ -1,21 +1,34 @@
 package com.dicoding.submissionMade.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.dicoding.submissionMade.R;
 import com.dicoding.submissionMade.adapter.ListTvShowAdapter;
 import com.dicoding.submissionMade.item.TvShow;
 import com.dicoding.submissionMade.viewModel.TvShowViewModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -27,6 +40,11 @@ public class TvShowFragment extends Fragment {
 
     private ListTvShowAdapter adapter;
     private ProgressBar progressBar;
+    private Context ctx;
+    private RecyclerView recyclerView;
+    private TvShowViewModel tvShowViewModel;
+    private SearchView searchViewTvShow;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public TvShowFragment() {
         // Required empty public constructor
@@ -39,20 +57,106 @@ public class TvShowFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_tv_show, container, false);
         progressBar = view.findViewById(R.id.progressBar2);
 
+        ctx = this.getContext();
+
         adapter = new ListTvShowAdapter();
-        RecyclerView recyclerView = view.findViewById(R.id.rv_tv_show);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView = view.findViewById(R.id.rv_tv_show);
+        recyclerView.setLayoutManager(new LinearLayoutManager(ctx));
         recyclerView.setAdapter(adapter);
 
-        TvShowViewModel tvShowViewModel = new ViewModelProvider(this).get(TvShowViewModel.class);
+        tvShowViewModel = new ViewModelProvider(this).get(TvShowViewModel.class);
         tvShowViewModel.getTvShow().observe(getViewLifecycleOwner(), getTvShow);
         tvShowViewModel.setTvShow();
 
         showLoading(true);
 
+        searchViewTvShow = view.findViewById(R.id.search_tv_show);
+        searchViewTvShow.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                showLoading(true);
+
+                final ArrayList<TvShow> filteredList = new ArrayList<>();
+                final String API_KEY = "d9c1d6e1b7d10d2ad0ac0c8e7e9abb81";
+                String url = "https://api.themoviedb.org/3/search/tv?api_key=" + API_KEY + "&language=en-US&query=" + query;
+
+                AndroidNetworking.get(url)
+                        .setPriority(Priority.LOW)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONArray list = response.getJSONArray("results");
+                                    for (int i = 0; i < list.length(); i++) {
+                                        JSONObject tvShow = list.getJSONObject(i);
+                                        TvShow tvShowItems = new TvShow(tvShow);
+                                        filteredList.add(tvShowItems);
+                                    }
+                                    adapter.setData(filteredList);
+                                } catch (JSONException e) {
+                                    Log.d("Exception", e.getMessage());
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+                                Log.d("onFailure", anError.getMessage());
+                            }
+                        });
+
+                showLoading(false);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                pullAndRefresh();
+            }
+        });
+
         return view;
 
     }
+
+    private void pullAndRefresh() {
+        swipeProgress(true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                recyclerView.setLayoutManager(new LinearLayoutManager(ctx));
+                recyclerView.setAdapter(adapter);
+                tvShowViewModel.setTvShow();
+
+                searchViewTvShow.setQuery("", false);
+                searchViewTvShow.clearFocus();
+
+                swipeProgress(false);
+            }
+        }, 3000);
+    }
+
+    private void swipeProgress(final boolean show) {
+        if (!show) {
+            swipeRefreshLayout.setRefreshing(show);
+            return;
+        }
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(show);
+            }
+        });
+    }
+
 
     private Observer<ArrayList<TvShow>> getTvShow = new Observer<ArrayList<TvShow>>() {
         @Override
