4bb33026b188ba15855081f21e8ea522947ce1d5
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-03-02 12:13:16 +0800

    notifikasi movie release nya gagal

--- app/src/main/java/com/dicoding/submissionMade/activity/NotificationSettingActivity.java
+++ app/src/main/java/com/dicoding/submissionMade/activity/NotificationSettingActivity.java
@@ -1,36 +1,23 @@
 package com.dicoding.submissionMade.activity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
-import android.util.Log;
 
-import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.common.Priority;
-import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.dicoding.submissionMade.R;
 import com.dicoding.submissionMade.item.Movie;
 import com.dicoding.submissionMade.notification.MovieDailyReceiver;
 import com.dicoding.submissionMade.notification.MovieUpcomingReceiver;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class NotificationSettingActivity extends PreferenceActivity {
 
 
-    private static final String API_KEY = "d9c1d6e1b7d10d2ad0ac0c8e7e9abb81";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +33,6 @@ public class NotificationSettingActivity extends PreferenceActivity {
         SwitchPreference mSwitchReminder;
         SwitchPreference mSwitchToday;
 
-        public class GetMovieTask extends AsyncTask<String, Void, Void> {
-            @Override
-            protected Void doInBackground(String... strings) {
-                getData(strings[0]);
-                return null;
-            }
-        }
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -66,7 +46,8 @@ public class NotificationSettingActivity extends PreferenceActivity {
                 }
             } else {
                 if (value) {
-                    setReleaseAlarm();
+                    mMovieUpcomingReceiver.setAlarm(getActivity());
+//                    setReleaseAlarm();
                 } else {
                     mMovieUpcomingReceiver.cancelAlarm(getActivity());
                 }
@@ -74,10 +55,6 @@ public class NotificationSettingActivity extends PreferenceActivity {
             return true;
         }
 
-        private void setReleaseAlarm() {
-            MainPreferenceFragment.GetMovieTask getDataAsync = new MainPreferenceFragment.GetMovieTask();
-            getDataAsync.execute("https://api.themoviedb.org/3/movie/upcoming?api_key=" + API_KEY + "&language=en-US");
-        }
 
         @Override
         public void onCreate(final Bundle savedInstanceState) {
@@ -90,37 +67,5 @@ public class NotificationSettingActivity extends PreferenceActivity {
             mSwitchToday.setOnPreferenceChangeListener(this);
         }
 
-        public void getData(String url) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-            Date date = new Date();
-            final String today = dateFormat.format(date);
-
-            AndroidNetworking.get(url)
-                    .setPriority(Priority.LOW)
-                    .build()
-                    .getAsJSONObject(new JSONObjectRequestListener() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                JSONArray list = response.getJSONArray("results");
-                                for (int i = 0; i < list.length(); i++) {
-                                    JSONObject movie = list.getJSONObject(i);
-                                    if (movie.getString("release_date").equals(today)) {
-                                        Movie movieItems = new Movie(movie);
-                                        mNotifList.add(movieItems);
-                                    }
-                                }
-                                mMovieUpcomingReceiver.setAlarm(getActivity(), mNotifList);
-                            } catch (JSONException e) {
-                                Log.d("Exception", e.getMessage());
-                            }
-                        }
-
-                        @Override
-                        public void onError(ANError anError) {
-                            Log.d("onFailure", anError.getMessage());
-                        }
-                    });
-        }
     }
 }
--- app/src/main/java/com/dicoding/submissionMade/notification/MovieUpcomingReceiver.java
+++ app/src/main/java/com/dicoding/submissionMade/notification/MovieUpcomingReceiver.java
@@ -11,20 +11,34 @@ import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.dicoding.submissionMade.R;
 import com.dicoding.submissionMade.item.Movie;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MovieUpcomingReceiver extends BroadcastReceiver {
 
     private static int mNotifId = 2000;
+    private static final String API_KEY = "d9c1d6e1b7d10d2ad0ac0c8e7e9abb81";
 
     private void sendNotification(Context context, String title, String mDesc, int id) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(
@@ -59,50 +73,84 @@ public class MovieUpcomingReceiver extends BroadcastReceiver {
         return PendingIntent.getBroadcast(context, 1011, intent, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
-    public void setAlarm(Context context, List<Movie> mMovieResults) {
+    public void setAlarm(Context context) {
         int delay = 0;
 
-        for (Movie movie : mMovieResults) {
-            cancelAlarm(context);
-            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            Intent intent = new Intent(context, MovieUpcomingReceiver.class);
-            intent.putExtra("movietitle", movie.getTitle());
-            intent.putExtra("movieposter", movie.getPoster());
-            intent.putExtra("moviemDescription", movie.getDescription());
-            intent.putExtra("id", mNotifId);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        cancelAlarm(context);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, MovieUpcomingReceiver.class);
+        intent.putExtra("id", mNotifId);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.HOUR_OF_DAY, 8);
-            calendar.set(Calendar.MINUTE, 0);
-            calendar.set(Calendar.SECOND, 0);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
 
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-                alarmManager.setInexactRepeating(
-                        AlarmManager.RTC_WAKEUP,
-                        calendar.getTimeInMillis() + delay,
-                        AlarmManager.INTERVAL_DAY,
-                        pendingIntent
-                );
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,
-                        calendar.getTimeInMillis() + delay, pendingIntent);
-            }
-            mNotifId += 1;
-            delay += 3000;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            alarmManager.setInexactRepeating(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.getTimeInMillis() + delay,
+                    AlarmManager.INTERVAL_DAY,
+                    pendingIntent
+            );
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,
+                    calendar.getTimeInMillis() + delay, pendingIntent);
         }
+        mNotifId += 1;
+        delay += 3000;
         Toast.makeText(context, "Upcoming Notif ON", Toast.LENGTH_SHORT).show();
     }
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        String mMovieTitle = intent.getStringExtra("movietitle");
-        int id = intent.getIntExtra("id", 0);
-        String mDesc = context.getString(R.string.today_release) + " " + mMovieTitle;
-        sendNotification(context, context.getString(R.string.app_name), mDesc, id);
+        List<Movie> movieList = getReleaseMovieToday("https://api.themoviedb.org/3/movie/upcoming?api_key=d9c1d6e1b7d10d2ad0ac0c8e7e9abb81&language=en-US");
+        if (movieList.size() > 0) {
+            for (Movie movie : movieList) {
+                int id = intent.getIntExtra("id", 0);
+                String mDesc = context.getString(R.string.today_release) + " " + movie.getTitle();
+                sendNotification(context, context.getString(R.string.app_name), mDesc, id);
+            }
 
+        }
+
+    }
+
+    public List<Movie> getReleaseMovieToday(String url) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        Date date = new Date();
+        final String today = dateFormat.format(date);
+        final ArrayList<Movie> mNotifList = new ArrayList<>();
+
+        AndroidNetworking.get(url)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray list = response.getJSONArray("results");
+                            for (int i = 0; i < list.length(); i++) {
+                                JSONObject movie = list.getJSONObject(i);
+                                if (movie.getString("release_date").equals(today)) {
+                                    Movie movieItems = new Movie(movie);
+                                    mNotifList.add(movieItems);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            Log.d("Exception", e.getMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("onFailure", anError.getMessage());
+                    }
+                });
+        return mNotifList;
     }
 
     public void cancelAlarm(Context context) {
